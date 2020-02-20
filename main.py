2a3ee5591ee22bbc383eeba32e4c60e5c817a0b3
leedsharkey@gmail.com <leedsharkey@gmail.com>
2020-02-20 15:18:25 +0100

    Added bigger architecture for DAN

--- main.py
+++ main.py
@@ -907,6 +907,28 @@ def finalize_args(parser):
                                    'mod_connect_dict': mod_connect_dict,
                                    'num_fc_channels': 16}
         vars(args)['energy_weight_mask'] = [1.0, 0.784, 7.84, 15.68]
+    elif args.architecture == 'DAN_med_5_layers_selftop':
+        vars(args)['state_sizes'] = [[args.batch_size,  1, 28, 28],
+                                     [args.batch_size, 32, 16, 16],
+                                     [args.batch_size, 32, 8, 8],
+                                     [args.batch_size, 100],
+                                     [args.batch_size, 50]]
+
+        mod_connect_dict = {0: [1],
+                            1: [0,2],
+                            2: [1,3],
+                            3: [2,4],
+                            4: [3,4]}
+
+        vars(args)['arch_dict'] = {'num_ch': 32,
+                                   'num_ch_initter': 16,
+                                   'num_sl': len(args.state_sizes) - 1,
+                                   'kernel_sizes': [3, 3, 3],
+                                   'strides': [1,1],
+                                   'padding': 1,
+                                   'mod_connect_dict': mod_connect_dict,
+                                   'num_fc_channels': 32}
+        vars(args)['energy_weight_mask'] = [10.45, 1.0, 4.0, 81.92, 163.84]
     elif args.architecture == 'DAN_small_4_layers':
         vars(args)['state_sizes'] = [[args.batch_size,  1, 28, 28],
                                      [args.batch_size, 32, 16, 16],
