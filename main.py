e264bb95112752ae813550dc823d9f01b4bbed19
leedsharkey@gmail.com <leedsharkey@gmail.com>
2020-02-24 10:53:47 +0100

    Added smaller large 5 layer archi

--- main.py
+++ main.py
@@ -1197,6 +1197,30 @@ def finalize_args(parser):
                                        'num_fc_channels': 128}
             vars(args)['energy_weight_mask'] = [1.0, 0.18, 0.48, 5.95, 24.0]
 
+        if args.architecture == 'DAN_cifar10_large_5_layers_top2self_fcconvconnect':
+            vars(args)['state_sizes'] = [[args.batch_size, 3, 32, 32],  # 3072
+                                         [args.batch_size, 64, 16, 16], # 16384
+                                         [args.batch_size, 32, 8, 8], # 6400
+                                         [args.batch_size, 516],
+                                         [args.batch_size, 128]]
+
+            mod_connect_dict = {0: [1],
+                                1: [0, 2],
+                                2: [1, 3, 4],
+                                3: [2, 3, 4],
+                                4: [3, 4]}
+
+            vars(args)['arch_dict'] = {'num_ch': 64,
+                                       'num_ch_initter': 64,
+                                       'num_sl': len(args.state_sizes) - 1,
+                                       'kernel_sizes': [3, 3, 3],
+                                       'strides': [1, 1],
+                                       'padding': 1,
+                                       'mod_connect_dict': mod_connect_dict,
+                                       'num_fc_channels': 64}
+            vars(args)['energy_weight_mask'] = [1.0, 0.18, 0.48, 5.95, 24.0]
+
+
         if args.architecture == 'DAN_cifar10_very_large_6_layers_top2self':
             vars(args)['state_sizes'] = [[args.batch_size, 3, 32, 32],  # 3072
                                          [args.batch_size, 64, 32, 32], # 65536
