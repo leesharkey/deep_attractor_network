315e96b5fde7040320f3f0c4d23323df076bcbb7
leedsharkey@gmail.com <leedsharkey@gmail.com>
2020-03-05 12:10:26 +0100

    detached contents of history to see if it helps new memory issue

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ class TrainingManager():
         self.max_history_len = 5000
         self.mean_neg_pos_margin = 100
         self.neg_it_schedule_cooldown = 0
-        self.cooldown_len = 2 #epochs
+        self.cooldown_len = 3 #epochs
         self.latest_pos_enrg = None
         self.latest_neg_enrg = None
         self.num_it_neg_mean = self.args.num_it_neg
@@ -349,9 +349,9 @@ class TrainingManager():
 
         # Save latest energy outputs so you can schedule the phase lengths
         if positive_phase:
-            self.latest_pos_enrg = energy
+            self.latest_pos_enrg = energy.detach()
         else:
-            self.latest_neg_enrg = energy
+            self.latest_neg_enrg = energy.detach()
 
 
         # Prepare gradients and sample for next sampling step
@@ -1087,30 +1087,6 @@ def finalize_args(parser):
                                    'num_fc_channels': 16}
         vars(args)['energy_weight_mask'] = [1.0, 0.784, 7.84, 15.68]
 
-
-    elif args.architecture == 'DAN_med_5_layers_self':
-        vars(args)['state_sizes'] = [[args.batch_size,  1, 28, 28],
-                                     [args.batch_size, 16, 28, 28],
-                                     [args.batch_size, 16, 8, 8],
-                                     [args.batch_size, 100],
-                                     [args.batch_size, 50]]
-
-        mod_connect_dict = {0: [0,1],
-                            1: [0,1,2],
-                            2: [1,2,3],
-                            3: [2,3,4],
-                            4: [3,4]}
-
-        vars(args)['arch_dict'] = {'num_ch': 16,
-                                   'num_ch_initter': 16,
-                                   'num_sl': len(args.state_sizes) - 1,
-                                   'kernel_sizes': [[3, 3], [3, 3], [3, 3], [3, 3]],
-                                   'strides': [1,1,1,1],
-                                   'padding': [[1,1], [1,1], [1,1], [1,1]],
-                                   'mod_connect_dict': mod_connect_dict,
-                                   'num_fc_channels': 16}
-        vars(args)['energy_weight_mask'] = [1.0, 0.0625, 0.784, 7.84, 15.68]
-
     elif args.architecture == 'DAN_very_small_4_layers_selftop_smallworld':
         vars(args)['state_sizes'] = [[args.batch_size,  1, 28, 28],
                                      [args.batch_size, 16, 8, 8],
@@ -1198,6 +1174,28 @@ def finalize_args(parser):
                                    'mod_connect_dict': mod_connect_dict,
                                    'num_fc_channels': 16}
         vars(args)['energy_weight_mask'] = [1.0, 0.784, 0.784, 7.84, 15.68]
+    elif args.architecture == 'DAN_med_5_layers_self':
+        vars(args)['state_sizes'] = [[args.batch_size,  1, 28, 28],
+                                     [args.batch_size, 16, 28, 28],
+                                     [args.batch_size, 16, 8, 8],
+                                     [args.batch_size, 100],
+                                     [args.batch_size, 50]]
+
+        mod_connect_dict = {0: [0,1],
+                            1: [0,1,2],
+                            2: [1,2,3],
+                            3: [2,3,4],
+                            4: [3,4]}
+
+        vars(args)['arch_dict'] = {'num_ch': 16,
+                                   'num_ch_initter': 16,
+                                   'num_sl': len(args.state_sizes) - 1,
+                                   'kernel_sizes': [[3, 3], [3, 3], [3, 3], [3, 3]],
+                                   'strides': [1,1,1,1],
+                                   'padding': [[1,1], [1,1], [1,1], [1,1]],
+                                   'mod_connect_dict': mod_connect_dict,
+                                   'num_fc_channels': 16}
+        vars(args)['energy_weight_mask'] = [1.0, 0.0625, 0.784, 7.84, 15.68]
 
     elif args.architecture == 'DAN_med_5_layers_selftop':
         vars(args)['state_sizes'] = [[args.batch_size,  1, 28, 28],
