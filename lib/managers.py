eb1670bcf01de8063ce9ac0e6208c4481189ea02
leedsharkey@gmail.com <leedsharkey@gmail.com>
2020-07-04 18:28:49 +0200

    Now saving ims during neg phase but only rarely

--- lib/managers.py
+++ lib/managers.py
@@ -306,12 +306,13 @@ class TrainingManager(Manager):
         for _ in tqdm(range(self.num_it_neg)):
             self.sampler_step(neg_states, neg_id, step=self.global_step)
             self.global_step += 1
-            # #TODO remove when done debugging viz
-            # neg_imgs_save = neg_states[0].detach().to('cpu')
-            # utils.save_image(neg_imgs_save,
-            #                  os.path.join(self.sample_log_dir,
-            #                               str(self.global_step)+'neg' + '.png'),
-            #                  nrow=16, normalize=True, range=(0, 1))
+            if self.batch_num - 10 % 1000 == 0:
+                #TODO remove when done debugging viz
+                neg_imgs_save = neg_states[0].detach().to('cpu')
+                utils.save_image(neg_imgs_save,
+                                 os.path.join(self.sample_log_dir,
+                                              str(self.global_step)+'neg' + '.png'),
+                                 nrow=16, normalize=True, range=(0, 1))
 
         # Stop calculting grads w.r.t. images
         for neg_state in neg_states:
